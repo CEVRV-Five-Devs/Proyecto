0c45c44a6fd3d4b6b620cb55f1ab9dd7e0c2aad1
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2024-03-12 14:05:02 +0100

    feat: update starlight, integrate sidebar

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,27 +1,93 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'Sounds of the Forest',
+      social: {
+        github: 'https://github.com/CEVRV-Five-Devs/Proyecto'
+      },
+      sidebar: [
+        {
+          label: 'Básicos',
+          items: [
+            { label: 'Título', link: '/basics/title' },
+            { label: 'Resumen', link: '/basics/brief' },
+            { label: 'Concepto', link: '/basics/concept' },
+            { label: 'Atractivo', link: '/basics/atractive' },
+            { label: 'Descripción', link: '/basics/description' },
+            { label: 'Historia', link: '/basics/story' }
+          ]
+        },
+        {
+          label: 'Interfaz',
+          items: [
+            { label: 'HUD', link: '/' },
+            { label: 'Menú', link: '/' },
+            { label: 'Inventario', link: '/' }
+          ]
+        },
+        {
+          label: 'Jugabilidad y reglas',
+          items: [
+            { label: 'Controles', link: '/' },
+            { label: 'Combate', link: '/' },
+            { label: 'Exploración', link: '/' },
+            { label: 'Enemigos', link: '/' },
+            { label: 'Otras mecánicas', link: '/' }
+          ]
+        },
+        {
+          label: 'Preproducción',
+          items: [
+            { label: 'Requisitos', link: '/' },
+            { label: 'Planificación', link: '/' },
+            { label: 'Presupuesto', link: '/' },
+            {
+              label: 'Herramientas',
+              items: [
+                { label: 'Software', link: '/' },
+                { label: 'Hardware', link: '/' }
+              ]
+            },
+            {
+              label: 'Desarrollo',
+              items: [
+                { label: 'Equipo', link: '/' },
+                { label: 'Metodología', link: '/' },
+                { label: 'Ciclo de vida', link: '/' }
+              ]
+            },
+            {
+              label: 'Lanzamiento',
+              items: [
+                { label: 'Recepción', link: '/' },
+                { label: 'Actualizaciones', link: '/' },
+                { label: 'Soporte', link: '/' }
+              ]
+            }
+          ]
+        },
+
+        {
+          label: 'Marketing',
+          items: [
+            { label: 'Público objetivo', link: '/' },
+            { label: 'Distribución', link: '/' },
+            { label: 'Monetización', link: '/' }
+          ]
+        },
+        {
+          label: 'El Juego',
+          items: [
+            { label: 'Profundidad', link: '/' },
+            { label: 'Progresión', link: '/' },
+            { label: 'Scope', link: '/' }
+          ]
+        }
+      ]
+    })
+  ]
+})
---
+++ docs/src/content/docs/basics/story.md
@@ -0,0 +1,12 @@
+---
+title: Welcome to Starlight
+description: Get started building your docs site with Starlight.
+---
+
+## Trama
+
+## Personajes
+
+## Escenarios
+
+## Referencias
